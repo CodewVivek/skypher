c9190669cf12aa6087a28a3092dfb38559c07e2b
CodewVivek <vivekmanikonda113@gmail.com>
2025-07-07 14:34:47 +0530

    search bar

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
-import { ExternalLink, Calendar, Tag } from 'lucide-react';
+import { ExternalLink, Calendar, Tag, Search } from 'lucide-react';
 import Like from '../Components/Like';
 import { useNavigate } from 'react-router-dom';
-import SearchBar from '../Components/SearchBar';
+
 
 const Dashboard = () => {
   const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+  const [search, setSearch] = useState('');
+  const projectsSearch = projects.filter(
+    project => project.name.toLowerCase().includes(search.toLowerCase()) ||
+      (project.category_type.toLowerCase().includes(search.toLowerCase())) ||
+      (project.description.toLowerCase().includes(search.toLowerCase()))
+  )
 
 
   useEffect(() => {
@@ -52,15 +58,46 @@ const Dashboard = () => {
     return <div className="min-h-screen flex items-center justify-center text-red-600">Error: {error}</div>;
   }
 
+
   return (
     <>
       <div className="min-h-screen mt-20">
         <div className='flex items-center justify-center'>
-          <SearchBar />
+          <div className="relative  justify-center w-full max-w-md">
+            <input
+              type="text"
+              placeholder="Search for Launches,categories,or more..."
+              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+              <Search />
+            </span>
+          </div>
         </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          {
+            projectsSearch.length == 0 && (
+              <div>
+                <div className="flex justify-center items-center min-h-[60vh] sm:min-h-[70vh]">
+                  <div className="flex flex-col justify-center items-center text-center">
+                    <svg data-v-ad307406="" xmlns="http://www.w3.org/2000/svg" width="144" height="144" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-animal-icon lucide-animal animal-icon"><path d="M13 16a3 3 0 0 1 2.24 5"></path><path d="M18 12h.01"></path><path d="M18 21h-8a4 4 0 0 1-4-4 7 7 0 0 1 7-7h.2L9.6 6.4a1 1 0 1 1 2.8-2.8L15.8 7h.2c3.3 0 6 2.7 6 6v1a2 2 0 0 1-2 2h-1a3 3 0 0 0-3 3"></path><path d="M20 8.54V4a2 2 0 1 0-4 0v3"></path><path d="M7.612 12.524a3 3 0 1 0-1.6 4.3"></path></svg>
+                    <h2 className="text-4xl sm:text-5xl font-bold text-gray-650 mb-4">
+                      No Launches Available
+                    </h2>
+                    <p className="text-lg text-gray-600">
+                      Currently, there are no  launches scheduled or listed.<br />
+                      We are continuously updating our records; please revisit shortly for new information.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            )
+          }
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {projects.map((project) => (
+            {projectsSearch.map((project) => (
 
               <div
                 key={project.id}
@@ -104,8 +141,8 @@ const Dashboard = () => {
                 </div>
               </div>
             ))}
-          </div>
-        </div>
+          </div >
+        </div >
       </div >
 
 
